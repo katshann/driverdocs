5938c85a552737490a848c916a27a6aac5ec81ec
Rob Dobson <rob@rdobson.co.uk>
2013-10-07 17:10:05 +0100

    Adding extract.py for providing a data source for populating an article.
    Signed-off-by: Rob Dobson <rob@rdobson.co.uk>

---
+++ articlegenerator/extract.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+"""Module for extracting article information from various data sources"""
+
+from driverfiles.models import *
+
+class ArticleDataSource(object):
+    """A base class for obtaining data to fill out an article template"""
+
+    data = None
+    
+    def __init__(self, data=None):
+        if data:
+            self.data = data
+
+    def export(self):
+        return self.data
+
+    def __add__(self, x):
+        new_dict = dict(self.data.items() + x.export().items())
+        return ArticleDataSource(new_dict)
+
+class DriverRepoDataSource(ArticleDataSource):
+    
+    def __init__(self, loc):
+        self.loc = loc
+
+    def get_inspector(self):
+        return DriverRepoPackage(self.loc)
+
+    def collect(self):
+        drp = self.get_inspector()
+        data_rec = {}
+
+        # Get ISO information
+        iso = drp.get_iso()
+        data_rec['iso'] = {
+                            'filename': iso.get_filename(),
+                            'md5': iso.get_md5(),
+                          } 
+                            
+        # Get Zip file information
+        zip_file = drp.get_zip()
+        data_rec['zip'] = {
+                            'filename': zip_file.get_filename(),
+                            'md5': zip_file.get_md5(),
+                          }
+
+        # Get kernel version
+        data_rec['kernel_version'] = drp.get_kernel_version()
+
+        # Get metadata md5
+        data_rec['metadata_md5'] = drp.get_metadata_md5()
+
+        # Get RPM information
+        rpm_data = []
+
+        driver_rpms, userspace_rpms = drp.get_rpms()
+        for rpm in driver_rpms + userspace_rpms:
+            rpm_rec = {
+                        'module_name': rpm.get_name(),
+                        'version': rpm.get_version(),
+                        'filename': rpm.get_filename(),
+                      }
+
+            if 'get_kernel' in dir(rpm):
+                rpm_rec['kernel'] = rpm.get_kernel()
+
+            rpm_data.append(rpm_rec)
+
+        data_rec['rpms'] = rpm_data
+
+        # Get driver information
+        driver_data = []
+
+        for rpm in [rpm for rpm in driver_rpms if rpm.get_kernel() == "xen"]:
+            driver_rec = {'name': rpm.get_name(), 'version': rpm.get_version()}
+            driver_data.append(driver_rec)
+
+        data_rec['drivers'] = driver_data
+
+        self.data = data_rec
+
